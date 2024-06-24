298a19916ec22e36e45c49d1cc32685c37fb1c56
Thomas COLLET <thomas.collet@axa.fr>
2024-06-24 09:06:54 +0200

    Le back_Pateint fonctionne complétement même sur docker

--- back_Patient/Program.cs
+++ back_Patient/Program.cs
@@ -1,8 +1,15 @@
+using back_Patient.Data;
+using back_Patient.Service;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -12,6 +19,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<PatientService>();
 
 var app = builder.Build();
 
